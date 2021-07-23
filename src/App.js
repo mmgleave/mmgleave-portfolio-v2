eaaf5a4035dea382058ed96e527a8d05aa7de1c1
General Kenobi <78252449+mmgleave@users.noreply.github.com>
2021-07-23 16:31:36 -0600

    basic app.js file contents

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+// components
+import About from './components/About';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Portfolio from './components/Portfolio';
+import Resume from './components/Resume';
+
 function App() {
+
+  const [projects] = useState([
+    {
+      image: '',
+      title: '',
+      link: '',
+      repo: ''
+    },
+    {
+      image: '',
+      title: '',
+      link: '',
+      repo: ''
+    },
+    {
+      image: '',
+      title: '',
+      link: '',
+      repo: ''
+    },
+    {
+      image: '',
+      title: '',
+      link: '',
+      repo: ''
+    },
+    {
+      image: '',
+      title: '',
+      link: '',
+      repo: ''
+    },
+    {
+      image: '',
+      title: '',
+      link: '',
+      repo: ''
+    }
+  ])
+
+  const [contactSelected, setContactSelected] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header></Header>
+      <main>
+        {!contactSelected ? (
+          <>
+            <About></About>
+            <Portfolio></Portfolio>
+            <Resume></Resume>
+          </>
+        ) : (
+          <Contact></Contact>
+        )}
+        <Footer></Footer>
+      </main>
     </div>
   );
 }
