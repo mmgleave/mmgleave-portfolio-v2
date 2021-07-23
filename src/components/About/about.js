78b75795371c83cf37935fa91d2ac03a130391a5
General Kenobi <78252449+mmgleave@users.noreply.github.com>
2021-07-23 16:43:39 -0600

    basic about me component

---
+++ src/components/About/about.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import profilePhoto from '../../assets/'
+
+function About() {
+    return (
+        <section className="">
+            <h1>About Me</h1>
+            <img 
+            src={profilePhoto}
+            className=""
+            style={{ width: "100%"}}
+            alt="profile photo"
+            />
+            <p>Bio text: sic mundus creatus est.</p>
+        </section>
+    )
+}
