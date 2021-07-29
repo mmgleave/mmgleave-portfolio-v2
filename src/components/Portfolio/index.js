b5fa3a7b6d44f4adae865d6bd5f3076f1e66ed76
General Kenobi <78252449+mmgleave@users.noreply.github.com>
2021-07-28 22:16:27 -0600

    basic porfolio structure

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,1 +1,24 @@
 import React from 'react';
+
+function Portfolio({ projects }) {
+    return (
+        <section>
+            {projects.map((project) => {
+                <div>
+                    <span>
+                        {project.title}
+                    </span>
+                    {/* <img
+                        src={require(``).default}
+                        alt={project.title}
+                    /> */}
+                    <div>
+                        <a href={project.link}/>
+                    </div>
+                </div>
+            })}
+        </section>
+    )
+};
+
+export default Portfolio;
