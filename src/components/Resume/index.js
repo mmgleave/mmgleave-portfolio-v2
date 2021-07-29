f81bd8300fb6752e4c7774eb4b52391b9c474b6f
General Kenobi <78252449+mmgleave@users.noreply.github.com>
2021-07-28 22:10:38 -0600

    add resume to files and resume component

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,1 +1,14 @@
 import React from 'react';
+import resume from '../../assets/resume/mg-resume-2021.pdf';
+
+function Resume() {
+    return(
+        <div>
+            <a href={resume} download="MichelleGleaveResume2021.pdf">
+                Download Resume
+            </a>
+        </div>
+    );
+};
+
+export default Resume;
